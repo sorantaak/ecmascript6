5a9477aa2f09d053b8649c212403a093ad753979
soran <soran.taak@gmail.com>
2023-01-04 10:19:20 +0330

    a const an let variable

---
+++ app.js
@@ -0,0 +1,18 @@
+function func() {
+	let x = 10;
+	if (true) {
+		let x = "soran";
+	}
+	console.log(x);
+}
+
+func();
+
+const NAMES = 4;
+
+// NAMES = "asrin";
+console.log(NAMES);
+
+const Nums = [1, 2, 3, 4];
+Nums.push(5);
+console.log(Nums);
